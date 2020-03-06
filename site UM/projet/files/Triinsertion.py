ade0191327b5fe9e6c68a2427c6dee0d37977a30
Ugo Maliczak <61891314+Malistik@users.noreply.github.com>
2020-03-06 23:58:53 +0100

    Add files via upload

---
+++ site UM/projet/files/Triinsertion.py
@@ -0,0 +1,14 @@
+﻿def tri_insertion(a):
+    liste = [10, 3, 7, 5, 6, 1]
+    N = len(A)
+    for i in range(1,n):
+        print(a)
+        L = a[i]
+        j = i-1
+        while j>=0 and A[j] > L:
+            a[j+1] = a[j]
+            j = j-1
+        a[j+1] = L
+
+
+
